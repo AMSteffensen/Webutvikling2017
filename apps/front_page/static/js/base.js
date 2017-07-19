ababe1cc7412e3b25a202ebafaf3f9565a4235a8
tehUsual <haugland54@gmail.com>
2017-07-19 15:10:40 +0200

    fixed script errors

--- apps/front_page/static/js/base.js
+++ apps/front_page/static/js/base.js
@@ -19,39 +19,3 @@ $(document).ready(function() {
   var loc = location.pathname.split("/");
       $('#menu-content a[href*="/' + loc[loc.length -3] + "/" + loc[loc.length -2] + '/"]').parents("ul:first").attr("aria-expanded",true).addClass("in");
   });
-
-  <!-- Hide/show searchbar -->
-
-    document.getElementById("searchBar").style.display = 'none';
-      function hideShow() {
-
-          var x = document.getElementById('searchBar');
-          if (x.style.display === 'none') {
-              x.style.display = 'block';
-          } else {
-              x.style.display = 'none';
-          }
-      }
-
-
-  // localStorage.setItem('shownOnRefresh', JSON.stringify(shownOnRefresh));
-  //
-  // $('#brukere').on('shown.bs.collapse', '.panel-collapse', function() {
-  //         shownOnRefresh = JSON.parse(localStorage.getItem('shownOnRefresh'));
-  //         if ($.inArray($(this).attr('id'), shownOnRefresh) == -1) {
-  //             shownOnRefresh.push($(this).attr('id'));
-  //         };
-  //         localStorage.setItem('shownOnRefresh', JSON.stringify(shownOnRefresh));
-  // });
-  //
-  // $('#brukere').on('hidden.bs.collapse', '.panel-collapse', function() {
-  //         shownOnRefresh = JSON.parse(localStorage.getItem('shownOnRefresh'));
-  //         shownOnRefresh.splice( $.inArray($(this).attr('id'), shownOnRefresh), 1 );//remove item from array
-  //         localStorage.setItem('shownOnRefresh', JSON.stringify(shownOnRefresh));
-  // });
-  //
-  // // On page refresh
-  // var shownOnRefresh = JSON.parse(localStorage.getItem('shownOnRefresh '));
-  // for (var i in shownOnRefresh ) {
-  //     $('#' + shownOnRefresh [i]).addClass('in');
-  // }
