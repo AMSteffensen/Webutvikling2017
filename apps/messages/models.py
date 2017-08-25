965587b59347093a5b52d89a6d3436d53c9f98ef
tehUsual <haugland54@gmail.com>
2017-08-25 13:28:39 +0200

    base for showing msgs in the notif bar

--- apps/messages/models.py
+++ apps/messages/models.py
@@ -1,10 +1,12 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-
+## MESSAGE RELATIONS
 class MessageRelationManager(models.Manager):
     def connections(self, userObj):
         return super(MessageRelationManager, self).get_queryset().filter(models.Q(userA=userObj) | models.Q(userB=userObj))
+    def unread(self, user_id):
+        return super(MessageRelationManager, self).get_queryset().filter(read=False)
 
 
 class MessageRelation(models.Model):
@@ -17,9 +19,9 @@ class MessageRelation(models.Model):
 
     def __str__(self):
         return '{} <-> {}'.format(self.userA, self.userB)
+## /MESSAGE RELATIONS
 
-
-
+## MESSAGES
 class MessageManager(models.Manager):
     def by_id(self, msg_id, sort_date=True, msg_count=20):
         if sort_date:
@@ -29,7 +31,6 @@ class MessageManager(models.Manager):
 
 
 class Message(models.Model):
-
     msg_id = models.ForeignKey(MessageRelation, related_name='msg_id')
     user_from = models.ForeignKey(User, related_name='msg_user_from')
     user_to = models.ForeignKey(User, related_name='msg_user_to')
@@ -41,3 +42,4 @@ class Message(models.Model):
 
     class Meta:
         ordering = ('created',)
+## /MESSAGES
